2ad6d0c23e7407e80ed57bfbf7cef4f5f5e7fb9d
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2026-01-04 18:03:34 +0900

    Added ex06

---
+++ ex06/Harl.hpp
@@ -0,0 +1,16 @@
+#ifndef EX06_HARL_HPP_
+#define EX06_HARL_HPP_
+
+#include <string>
+
+class Harl {
+  void debug();
+  void info();
+  void warning();
+  void error();
+
+public:
+  void complain(std::string level);
+};
+
+#endif // !EX06_HARL_HPP_
---
+++ ex06/main.cpp
@@ -0,0 +1,56 @@
+#include "Harl.hpp"
+#include <iostream>
+#include <string>
+
+#define DEBUG 0
+#define INFO 1
+#define WARNING 2
+#define ERROR 3
+
+namespace {
+int get_corresponding_level(std::string level) {
+  static std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+  for (int i = 0; i < 4; i++) {
+    if (levels[i] == level) {
+      return i;
+    }
+  }
+  return -1;
+}
+} // namespace
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: ./harl_filter <level> (e.g WARNING)" << std::endl;
+    return 1;
+  }
+  int level = get_corresponding_level(argv[1]);
+  if (level == -1) {
+    std::cout << "[ Probably complaining about insignificant problems ]"
+              << std::endl;
+    return 0;
+  }
+
+  Harl harl;
+  switch (level) {
+  case DEBUG: // FALL THROUGH !!
+    std::cout << "[ DEBUG ]" << std::endl;
+    harl.complain("DEBUG");
+    std::cout << '\n';
+
+  case INFO: // FALL THROUGH !!
+    std::cout << "[ INFO ]" << std::endl;
+    harl.complain("INFO");
+    std::cout << '\n';
+
+  case WARNING: // FALL THROUGH !!
+    std::cout << "[ WARNING ]" << std::endl;
+    harl.complain("WARNING");
+    std::cout << '\n';
+
+  case ERROR: // FALL THROUGH !!
+    std::cout << "[ ERROR ]" << std::endl;
+    harl.complain("ERROR");
+    std::cout << '\n';
+  }
+}
